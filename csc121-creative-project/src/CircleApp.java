a0e184bc660045a786f84e39749921b6f7ee0199
sa-mensah21 <104472677+sa-mensah21@users.noreply.github.com>
2023-09-15 17:43:32 -0400

    +2 circles
    I added code that creates 2 more circle worlds and update them, however it does not render yet

--- csc121-creative-project/src/CircleApp.java
+++ csc121-creative-project/src/CircleApp.java
@@ -6,6 +6,8 @@ import processing.event.*;
  */
 public class CircleApp extends PApplet {
     CircleWorld w;
+    CircleWorld w2;
+    CircleWorld w3;
     
     public void settings() {
         this.size(400, 400);
@@ -13,15 +15,27 @@ public class CircleApp extends PApplet {
     
     public void setup() {
         w = new CircleWorld(200, 0);
+        w2 = new CircleWorld(300,0);
+        w3 = new CircleWorld(100,0);
+        
     }
     
     public void draw() {
         w = w.update();
         w.draw(this);
+        
+        w2 = w2.update();
+        w2.draw(this);
+        
+        w3 = w3.update();
+        w3.draw(this);
+        
+        
     }
     
     public void mousePressed(MouseEvent mev) {
         w = w.mousePressed(mev);
+
     }
     
     public void keyPressed(KeyEvent kev) {
